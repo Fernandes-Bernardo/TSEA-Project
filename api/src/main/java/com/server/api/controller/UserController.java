525adc57d0441c17814364923831048b5dafbee9
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-05-04 16:37:16 -0300

    feat: creating an controller to User, and a endpoint to get all users

---
+++ api/src/main/java/com/server/api/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.server.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.server.api.model.User;
+import com.server.api.service.UserService;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("allEmployees")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+}
