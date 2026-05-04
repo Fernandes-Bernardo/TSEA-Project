20ede49dd68a04f085a70e358bd5b8d642289f3a
Fernandes-Bernardo <bernardofpdev@gmail.com>
2026-05-04 15:59:22 -0300

    feat: creating User model

---
+++ api/src/main/java/com/server/api/model/User.java
@@ -0,0 +1,96 @@
+package com.server.api.model;
+
+import java.util.UUID;
+import org.hibernate.annotations.UuidGenerator;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "employeeId", nullable = false, unique = true)
+    private Integer employeeId;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "sector", nullable = false)
+    private String sector;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Builders
+
+    public User() {
+        id = UUID.randomUUID();
+    }
+
+    public User(String name, Integer employeeID, String role, String sector, String password) {
+        id = UUID.randomUUID();
+        this.name = name;
+        this.employeeId = employeeID;
+        this.role = role;
+        this.sector = sector;
+        this.password = password;
+    }
+
+    // Getters and Setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Integer employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getSector() {
+        return sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }    
+}
